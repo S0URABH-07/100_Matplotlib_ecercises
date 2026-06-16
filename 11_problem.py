9b70616ee775e6f2af1a69aabc0eb28f12355012
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-16 23:04:39 +0530

    Increse linewidth and Make dotted boundry

---
+++ 11_problem.py
@@ -0,0 +1,10 @@
+# Draw Bar plot and add bar boundry color and increse linewidth and make dotted boundry
+import matplotlib.pyplot as plt
+x = ["a","b","c","d","e","f"]
+y = [ 10,15,20,25,30,35]
+plt.xlabel("Name",fontsize=10)
+plt.ylabel("No.",fontsize=15)
+plt.title("Bar Plot")
+c = ["y",'b','r',"g"]
+plt.bar(x,y,width=0.5,color=c,align="edge",edgecolor="r",linewidth=5 , linestyle=":")
+plt.show()
