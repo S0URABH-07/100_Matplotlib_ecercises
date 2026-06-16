62bca9daa4b5a97ffc6c1fbd91e5cb9504cfdd99
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-16 20:23:34 +0530

    Draw bar plot and move x axis name

---
+++ 8_problem.py
@@ -0,0 +1,10 @@
+# Draw Bar plot and move x axis name 
+import matplotlib.pyplot as plt
+x = ["a","b","c","d","e","f"]
+y = [ 10,15,20,25,30,35]
+plt.xlabel("Name",fontsize=10)
+plt.ylabel("No.",fontsize=15)
+plt.title("Bar Plot")
+c = ["y",'b','r',"g"]
+plt.bar(x,y,width=0.5,color=c,align="edge")
+plt.show()
