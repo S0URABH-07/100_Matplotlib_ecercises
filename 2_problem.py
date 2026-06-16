ec08abb8ad1eb638485b48fc1d907ab2d36bfe38
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-16 23:29:22 +0530

    Drow bar plot and write label

--- 2_problem.py
+++ 2_problem.py
@@ -1,7 +1,7 @@
 # Draw Bar plot and write label
 import matplotlib.pyplot as plt
-x = ["a","b","c","d","e","f"]
-y = [ 10,15,20,25,30,35]
+x = ["a","b","c","d"]
+y = [ 10,15,20,25]
 plt.xlabel("Name")
 plt.ylabel("No.")
 plt.bar(x,y)
