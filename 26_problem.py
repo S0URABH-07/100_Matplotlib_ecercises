ca369b9e035d7988ab67ca8ec4c0505008cebeb5
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-17 23:41:27 +0530

    Plot Two Scatter Datasets

---
+++ 26_problem.py
@@ -0,0 +1,13 @@
+# Plot Two Scatter Datasets
+import matplotlib.pyplot as plt
+x1 = [1, 2, 3]
+y1 = [4, 5, 6]
+
+x2 = [1, 2, 3]
+y2 = [7, 3, 5]
+
+plt.scatter(x1, y1, color="blue", label="Class A",s=200)
+plt.scatter(x2, y2, color="green", label="Class B",s=200)
+
+plt.legend()
+plt.show()
