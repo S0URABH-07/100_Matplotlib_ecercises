0d8397e715da846b36dc6cb90050915b1cc334fa
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-17 22:46:37 +0530

    Show language name and rotate USE->rotation

---
+++ 18_problem.py
@@ -0,0 +1,23 @@
+# Draw two Bar plot without overlape and show Language names using xticks and rotate(tilt) the all names
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = ["python","c","c++","java"]
+y = [10,20,30,40]
+z = [80,25,48,12]
+
+width = 0.3
+p = np.arange(len(x))
+p1 = [j+width for j in p]
+
+plt.xlabel("language" , fontsize=15)
+plt.ylabel("No.",fontsize=15)
+plt.title("Bar Plot",fontsize=20)
+
+plt.bar(p,y,width,color='r',label="Chart 1")
+plt.bar(p1,z,width,color='y',label="Chart 2")
+
+plt.xticks(p+width/2, x,rotation=50)
+plt.legend()
+
+plt.show()
