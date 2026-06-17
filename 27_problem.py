16b2d9771ad77e4eddb717746a8e70026ee27f5e
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-17 23:43:37 +0530

    Combine Multiple Parameters & Draw Scatter

---
+++ 27_problem.py
@@ -0,0 +1,15 @@
+# Combine multiple parameter And Draw Scatter Chart
+import matplotlib.pyplot as plt
+
+x = [2, 3, 7, 2, 5, 9, 4, 6]
+y = [8, 1, 3, 4, 6, 1, 0, 5]
+
+plt.scatter(x,y,color="purple",s=150,marker="*",alpha=0.7,label="Data Points")
+
+plt.title("Customized Scatter Plot")
+plt.xlabel("X Values")
+plt.ylabel("Y Values")
+plt.grid(True)
+plt.legend()
+
+plt.show()
