d009cd21371b9e2940c1821c7939fa1fc8c72cac
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-17 22:37:46 +0530

    Drae two bar plot in one code without overlape

---
+++ 16_problem.py
@@ -0,0 +1,21 @@
+# Draw two Bar plot without overlape
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = ["python","c","c++","java"]
+y = [10,20,30,40]
+z = [80,25,48,12]
+
+width = 0.3
+p = np.arange(len(x))
+p1 = [j+width for j in p]
+
+plt.xlabel("language" , fontsize=15)
+plt.ylabel("No.",fontsize=15)
+plt.title("Bar Plot",fontsize=20)
+
+plt.bar(p,y,width,color='r',label="Chart 1")
+plt.bar(p1,z,width,color='y',label="Chart 2")
+plt.legend()
+
+plt.show()
