c4aee8eed17c73194918e0faa62eee46d8a38062
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-18 21:25:15 +0530

    Multiple Marker Styles Together

---
+++ 42_problem.py
@@ -0,0 +1,8 @@
+# Multiple Marker Styles Together
+import matplotlib.pyplot as plt
+plt.scatter([1, 2], [3, 4], marker="o", s=200, label="Circle")
+plt.scatter([3, 4], [5, 6], marker="s", s=200, label="Square")
+plt.scatter([5, 6], [7, 8], marker="^", s=200, label="Triangle")
+
+plt.legend()
+plt.show()
