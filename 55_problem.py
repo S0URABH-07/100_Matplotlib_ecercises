1c8ea7f35bb2042641c3d93ab7e455986ea48870
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-19 23:36:18 +0530

    Density Histogram

---
+++ 55_problem.py
@@ -0,0 +1,13 @@
+# Density Histogram
+import matplotlib.pyplot as plt
+
+scores = [55,60,62,65,68,70,72,75,78,80,82,85]
+
+plt.hist(
+    scores,
+    bins=6,
+    density=True,
+    color="red"
+)
+
+plt.show()
