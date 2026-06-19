6442b1326fe45c2196a502d56f555af5c376c604
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-19 23:26:36 +0530

    Specify the no. of bins

--- 51_problem.py
+++ 51_problem.py
@@ -1,7 +1,8 @@
-# Histogram Chart 
+# Specify the Number of Bins
 import matplotlib.pyplot as plt
-marks = [45, 50, 100, 60, 60, 65, 70, 75, 80, 85, 90]
 
-plt.hist(marks)
+marks = [45, 50, 55, 60, 60, 65, 70, 75, 80, 85, 90]
+
+plt.hist(marks, bins=5)
 
 plt.show()
--- 53_problem.py
+++
@@ -1,8 +0,0 @@
-# Specify the Number of Bins
-import matplotlib.pyplot as plt
-
-marks = [45, 45,50,60,70,77, 70, 75, 80, 85, 90]
-
-plt.hist(marks, bins=5)
-# bins=5 ->It controls how many groups the data is divided into.
-plt.show()
