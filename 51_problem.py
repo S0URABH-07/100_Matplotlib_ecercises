455db98a16e1469294c978e8070907945e8541bd
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-19 23:18:00 +0530

    Histogram Chart

---
+++ 51_problem.py
@@ -0,0 +1,7 @@
+# Histogram Chart 
+import matplotlib.pyplot as plt
+marks = [45, 50, 100, 60, 60, 65, 70, 75, 80, 85, 90]
+
+plt.hist(marks)
+
+plt.show()
---
+++ 52_problem.py
@@ -0,0 +1,12 @@
+# 
+import matplotlib.pyplot as plt
+marks = [45, 50, 100, 60, 60, 65, 70, 75, 80, 89, 90]
+
+plt.hist(
+    marks,
+    bins=5,
+    edgecolor="black",
+    color="skyblue"
+)
+
+plt.show()
