46efcedc911891820d850d24c059ea232714d806
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-21 23:09:34 +0530

    Pie chart and explode(slice) the part of c and change color

--- 69_problem.py
+++ 69_problem.py
@@ -1,8 +1,9 @@
-# Pie chart and explode the part of c
+# Pie chart and explode the part of c and change colors
 import matplotlib.pyplot as plt
 x = [10,20,30,40]
 y = ["c","c++","java","python"]
 z = [0.1,0.0,0.0,0.0]
+clr = ["b","y","g","r"]
 
-plt.pie(x,labels=y, explode=z)
+plt.pie(x,labels=y, explode=z, colors=clr)
 plt.show()
