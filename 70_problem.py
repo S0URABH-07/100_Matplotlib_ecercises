59d075197318794ccb2224d04f619e19cdf1eba6
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-21 23:11:55 +0530

    Pie chart and show percentages

---
+++ 70_problem.py
@@ -0,0 +1,9 @@
+# # Pie chart and show percentages
+import matplotlib.pyplot as plt
+x = [10,20,30,40]
+y = ["c","c++","java","python"]
+z = [0.1,0.0,0.0,0.0]
+clr = ["b","y","g","r"]
+
+plt.pie(x,labels=y, explode=z, colors=clr, autopct="%0d%%")
+plt.show()
