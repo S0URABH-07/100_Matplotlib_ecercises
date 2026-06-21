5acf3ac9706048c6d31f45e01ca8f33dea3ae557
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-21 23:40:36 +0530

    Pie chart and change line width

--- 77_problem.py
+++ 77_problem.py
@@ -5,5 +5,5 @@ y = ["c","c++","java","python"]
 z = [0.1,0.0,0.0,0.0]
 clr = ["b","y","g","r"]
 
-plt.pie(x,labels=y, explode=z, colors=clr, autopct="%0d%%" , shadow=True , radius=1.1 , startangle=90 , textprops={"fontsize":15} ,wedgeprops={"linewidth":10})
+plt.pie(x,labels=y, explode=z, colors=clr, autopct="%0d%%" , shadow=True , radius=1.1 , startangle=90 ,counterclock=False ,textprops={"fontsize":15} ,wedgeprops={"linewidth":10 , "edgecolor":"black"})
 plt.show()
