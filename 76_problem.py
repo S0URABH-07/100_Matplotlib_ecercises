8eb896bc22aeba0baa3796c6f5e6b0579aed18e6
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-21 23:29:52 +0530

    Make pie chart clock wise USE-> counterclock

---
+++ 76_problem.py
@@ -0,0 +1,9 @@
+# Make pie chart clock wise USE-> counterclock
+import matplotlib.pyplot as plt
+x = [10,20,30,40]
+y = ["c","c++","java","python"]
+z = [0.1,0.0,0.0,0.0]
+clr = ["b","y","g","r"]
+
+plt.pie(x,labels=y, explode=z, colors=clr, autopct="%0d%%" , shadow=True , radius=1.1 , startangle=90 , textprops={"fontsize":15} , counterclock=False)
+plt.show()
