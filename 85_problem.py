a530c5730a6909da74c0aa19638ff4b2be251c99
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-22 18:16:31 +0530

    Stem plot chart and change base clr or add label

---
+++ 85_problem.py
@@ -0,0 +1,8 @@
+# Stem plot chart and change base clr or add label
+import matplotlib.pyplot as plt
+x = [1,2,3,4,5,6]
+y = [6,2,3,5,1,3]
+
+plt.stem(x,y , linefmt=":", markerfmt="r" , bottom=4, basefmt="g", label="Stem plot")
+plt.legend(loc=2)
+plt.show()
