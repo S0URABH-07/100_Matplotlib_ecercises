ed25718899bd27f4e8f472bfd300907c14500c91
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-22 19:49:37 +0530

    Box plot and show mean point or outlier and change outlier color

--- 89_problem.py
+++ 89_problem.py
@@ -1,6 +1,6 @@
-# Box plot and show mean point or outlier
+# Box plot and show mean point or outlier and change outlier color
 import matplotlib.pyplot as plt
 x = [10,20,30,40,50,60,70,125]
 
-plt.boxplot(x ,labels=["python"] , patch_artist=True , showmeans=True)
+plt.boxplot(x ,labels=["python"] , patch_artist=True , showmeans=True , sym="r")
 plt.show()
