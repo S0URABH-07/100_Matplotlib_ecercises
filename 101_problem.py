0435ae93b1808a69b8664957419d983a0a76574f
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-22 23:49:02 +0530

    Fill_between plot in linear plot using Numpy

---
+++ 101_problem.py
@@ -0,0 +1,14 @@
+# Fill_between plot with numpy
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.array([1,2,3,4,5])
+y = np.array([1,2,3,4,5])
+
+plt.plot(x,y,color="r")
+plt.fill_between(x,y,color="y" , where=(x>=2) & (x<=4),alpha=0.4)
+
+plt.title("Fill_between plot")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.grid()
+plt.show()
