74cd903c56434ed6cf5a4c8a92f2244c2b963db4
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-22 23:19:03 +0530

    Step Plot with label and change color of step

---
+++ 99_problem.py
@@ -0,0 +1,12 @@
+# Step plot and add label or change step color and add grid()
+import matplotlib.pyplot as plt
+x = [5,10,15,20,25,30]
+y = [1,6,3,2,5,4]
+
+plt.step(x , y ,color="r" , marker="o" , ms=10 , mfc="b" , label="Step")
+plt.title("Step plot")
+plt.xlabel("x-axis")
+plt.ylabel("Y-axis")
+plt.legend(loc=4)
+plt.grid()
+plt.show()
