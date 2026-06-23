31df467189895930974469a51fc03a004a4aaedb
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-23 23:47:59 +0530

    Save Box plot in png

---
+++ 113_problem.py
@@ -0,0 +1,10 @@
+# Draw two Box plot
+import matplotlib.pyplot as plt
+x = [10,20,30,40,50,60,70,150]
+y = [20,40,60,80,100,120,140]
+
+list = [x,y]
+plt.boxplot(list ,labels=["python","C++"] , patch_artist=True)
+
+plt.savefig("Boxplot")
+plt.show()
