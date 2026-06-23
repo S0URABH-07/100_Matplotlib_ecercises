47603ef0c5fe7283c4eeb0bd7247e616aa314b54
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-23 23:15:35 +0530

    Save figure with Padding

---
+++ 105_problem.py
@@ -0,0 +1,22 @@
+# Draw two Bar plot without overlape
+# Save Figure with padding (boundry)
+import matplotlib.pyplot as plt
+import numpy as np
+
+subjects = ["Math", "Science", "English", "History"]
+
+student1 = [80, 75, 90, 85]
+student2 = [70, 85, 88, 80]
+
+x = np.arange(len(subjects))
+width = 0.35
+plt.bar(x - width/2, student1, width=width, label="Student 1")
+plt.bar(x + width/2, student2, width=width, label="Student 2")
+
+plt.xlabel("Subjects")
+plt.ylabel("Marks")
+plt.title("Student Marks Comparison")
+plt.xticks(x, subjects)
+plt.legend()
+plt.savefig("TwoBarPlot",dpi=500,facecolor="r")
+plt.show()
