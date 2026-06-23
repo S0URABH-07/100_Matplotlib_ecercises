881a2964ee9e9bf8ecd3d60c4bae73d78aec6b21
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-23 23:34:50 +0530

    Save figure with transparency

---
+++ 108_problem.py
@@ -0,0 +1,26 @@
+# Use Top diffrent parameters and Draw Bar Graph
+# Save figure in png with transparency
+import matplotlib.pyplot as plt
+
+algorithms = ["Logistic Regression","Decision Tree","Random Forest","XGBoost"]
+
+accuracy = [84, 88, 93, 95]
+
+bars = plt.bar(algorithms,accuracy,color=["gray", "orange", "green", "blue"],edgecolor="black",linewidth=1.5)
+
+for bar in bars:
+    plt.text(
+        bar.get_x() + bar.get_width()/2,
+        bar.get_height(),
+        f"{bar.get_height()}%",
+        ha="center",
+        va="bottom"
+    )
+
+plt.ylim(0, 100)
+plt.ylabel("Accuracy (%)")
+plt.title("Machine Learning Model Accuracy")
+plt.grid(axis="y", linestyle=":")
+
+plt.savefig("Bar_Graph",dpi=800,facecolor="y",transparent=True)
+plt.show()
