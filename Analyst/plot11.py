92a85ea19c0e8c83fa8563b5a6f1ca5a44063de0
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-07-26 22:27:27 +0530

    bar Plot

---
+++ Analyst/plot11.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("HR_Analytics.csv")
+
+bins = [18, 25, 35, 45, 55, 65]
+
+labels = [
+    "18-25",
+    "26-35",
+    "36-45",
+    "46-55",
+    "56-65"
+]
+
+df["AgeGroup"] = pd.cut(df["Age"],bins=bins,labels=labels)
+
+attrition_rate = (df.groupby("AgeGroup")["Attrition"].apply(lambda x: (x == "Yes").mean() * 100).reset_index(name="AttritionRate"))
+
+print(attrition_rate)
+
+plt.figure(figsize=(8,5))
+
+plt.bar(attrition_rate["AgeGroup"],attrition_rate["AttritionRate"])
+
+plt.title("Attrition Rate by Age Group")
+
+plt.xlabel("Age Group")
+
+plt.ylabel("Attrition Rate (%)")
+
+plt.grid(axis="y", linestyle="--", alpha=0.4)
+
+plt.show()
